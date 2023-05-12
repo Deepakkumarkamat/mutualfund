4915809b3547bbfc320b61db29b891ea8e234cd7
Deepak Kamat <krkamatd@gmail.com>
2023-05-12 17:33:40 +0530

    changes in all funddetail.ts

--- src/app/components/allfunds/allfunds.component.ts
+++ src/app/components/allfunds/allfunds.component.ts
@@ -4,27 +4,27 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-allfunds',
   templateUrl: './allfunds.component.html',
-  styleUrls: ['./allfunds.component.css']
+  styleUrls: ['./allfunds.component.css'],
 })
 export class AllfundsComponent {
-detsilList:any[]=[]
-allBottomDetail:any[]=[]
-data:any;
-// selectedFund:fund;
-constructor(private api:ApiService,private router:Router){}
-ngOnInit(){
-  this.api.getTopDetail().subscribe(res=>{
-    this.detsilList=res;
-    console.log(this.detsilList)
-  })
-  this.api.getBottomDetail().subscribe(res=>{
-    this.allBottomDetail =res;
-  })
-}
-fetchById(schemaId:number):void{
-  this.api.detailById(schemaId).subscribe((data)=>{
-    this.data = data;
-    console.log(data[27]);
-  })
-}
+  detsilList: any[] = [];
+  allBottomDetail: any[] = [];
+  data: any;
+  // selectedFund:fund;
+  constructor(private api: ApiService, private router: Router) {}
+  ngOnInit() {
+    this.api.getTopDetail().subscribe((res) => {
+      this.detsilList = res;
+      console.log(this.detsilList);
+    });
+    this.api.getBottomDetail().subscribe((res) => {
+      this.allBottomDetail = res;
+    });
+  }
+  fetchById(schemaId: number): void {
+    this.api.detailById(schemaId).subscribe((data) => {
+      this.data = data;
+      console.log(data[27]);
+    });
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,25 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private http:HttpClient) { }
-  getTopDetail(){
-    return this.http.get("http://localhost:8090/mutualfunds/details/top?limit=5").pipe(map((res:any)=>{
-      return res;
-    }))
+  constructor(private http: HttpClient) {}
+  getTopDetail() {
+    return this.http
+      .get('http://localhost:8090/mutualfunds/details/top?limit=5')
+      .pipe(
+        map((res: any) => {
+          return res;
+          
+        })
+      );
   }
-  getBottomDetail(){
-    return this.http.get("http://localhost:8090/mutualfunds/details/bottom?limit=5").pipe(map((res:any)=>{
-      return res;
-    }))
+  getBottomDetail() {
+    return this.http
+      .get('http://localhost:8090/mutualfunds/details/bottom?limit=5')
+      .pipe(
+        map((res: any) => {
+          return res;
+        })
+      );
   }
 
-  detailById(schemaId:number):Observable<any>{
-    return this.http.get<any>(`http://localhost:8090/mutualfunds/details/byid?id=${schemaId}`);
+  detailById(schemaId: number): Observable<any> {
+    return this.http.get<any>(
+      `http://localhost:8090/mutualfunds/details/byid?id=${schemaId}`
+    );
   }
 }
