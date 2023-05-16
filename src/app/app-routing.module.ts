1b7985443de8c1066b39ce74bbb7e62f81ff2f9b
sinha28shivam <shivam.sinha@hcl.com>
2023-05-16 20:14:23 +0530

    changes in Dashboard css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { WalletComponent } from './components/wallet/wallet.component';
-import { DepositPageComponent } from './components/wallet/deposit/deposit-page/deposit-page.component';
-import { WithdrawPageComponent } from './components/wallet/withdraw/withdraw-page/withdraw-page.component';
-import { TransactionPageComponent } from './components/wallet/transaction/transaction-page/transaction-page.component';
-import { WishlistPageComponent } from './components/wishlist/wishlist-page/wishlist-page.component';
-import { AllfundsComponent } from './components/allfunds/allfunds.component';
-import { AllfundsdetailComponent } from './components/allfundsdetail/allfundsdetail.component';
+// import { DepositPageComponent } from './components/wallet/deposit/deposit-page/deposit-page.component';
+// import { WithdrawPageComponent } from './components/wallet/withdraw/withdraw-page/withdraw-page.component';
+// import { TransactionPageComponent } from './components/wallet/transaction/transaction-page/transaction-page.component';
+// import { WishlistPageComponent } from './components/wishlist/wishlist-page/wishlist-page.component';
+// import { AllfundsComponent } from './components/allfunds/allfunds.component';
+// import { AllfundsdetailComponent } from './components/allfundsdetail/allfundsdetail.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+// import { LoginComponent } from './components/login/login.component';
+// import { RegisterComponent } from './components/register/register.component';
+// import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
+// import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { InvestNowComponent } from './components/invest-now/invest-now.component';
@@ -22,7 +22,7 @@ import { AccountDetailsComponent } from './components/account-details/account-de
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BuyingComponent } from './components/buying/buying.component';
 import { SipComponent } from './components/sip/sip.component';
-import { combineLatest } from 'rxjs';
+// import { combineLatest } from 'rxjs';
 
 const routes: Routes = [
   // {path:'',component:AllfundsComponent},
