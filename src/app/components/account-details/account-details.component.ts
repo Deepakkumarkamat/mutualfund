79b1004a54491330b498bdd36b4e1593f0e847a1
sinha28shivam <shivam.sinha@hcl.com>
2023-05-19 17:17:14 +0530

    Changes in Dashboard and allfunds .

--- src/app/components/account-details/account-details.component.ts
+++ src/app/components/account-details/account-details.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-details.component.css']
 })
 export class AccountDetailsComponent {
-
+ 
 }
+window.addEventListener("click", function(event: MouseEvent) {
+  if (!(event.target instanceof Element && event.target.matches('.account-button'))) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i] as HTMLElement;
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+});
